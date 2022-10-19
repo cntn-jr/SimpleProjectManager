e50f05f404351544a8df029fa71e2c07ba26e082
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2022-10-19 12:21:57 +0900

    add: theme of application
    １．使用する色を記述しました
     Changes to be committed:
    	new file:   src/resources/ts/App.tsx
    	new file:   src/resources/ts/index.tsx
    	new file:   src/resources/ts/theme/theme.ts

---
+++ src/resources/ts/App.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "./theme/theme";
+
+export default function App() {
+  return (
+      <ChakraProvider theme={theme}>
+        aaaaaaaaaaa
+      </ChakraProvider>
+  );
+}
---
+++ src/resources/ts/theme/theme.ts
@@ -0,0 +1,34 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "#03045E",
+        color: "#DEDEDE"
+      }
+    }
+  },
+  colors: {
+    main: {
+      1: "#03045E",
+      2: {
+        100: "#023E8A",
+        50: "background: rgba(2, 62, 138, 0.5)",
+      }
+    },
+    sub: {
+      1: "#0077B6",
+      2: "#0096C7",
+    },
+    font: {
+      100: "#DEDEDE",
+      70: "rgba(222, 222, 222, 0.7)",
+    },
+    high: "#F56565",
+    middle: "#ECC94B",
+    low: "#38A169",
+  }
+});
+
+export default theme;
