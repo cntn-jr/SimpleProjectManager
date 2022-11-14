b773cc4b87349c17665ca89edd8d4fbe9ebf33cc
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2022-11-14 12:03:57 +0900

    add: sign up function
    １．サインアップする画面を作成しました。
    ２．ログイン画面にサインアップ画面に遷移する機能を追加しました。
    ３．ユーザを作成する機能を作成しました。
     Changes to be committed:
    	modified:   src/app/Http/Controllers/LoginController.php
    	modified:   src/app/Models/User.php
    	modified:   src/resources/ts/components/molecules/LoginForm.tsx
    	new file:   src/resources/ts/components/molecules/SignForm.tsx
    	new file:   src/resources/ts/components/pages/Signup.tsx
    	modified:   src/resources/ts/router/Router.tsx
    	modified:   src/routes/api.php

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -18,8 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
+        'first_name',
+        'last_name',
         'password',
     ];
 
