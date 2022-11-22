342059bef01bbdf0356d30ac59aad241b5cbe6ad
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2022-11-22 11:21:56 +0900

    update: task controller
    １．タスクをユーザIDを元に取得する際に'1'で固定していたものをログインユーザのIDから取得するように変更しました。
    ２．タスクの追加や更新処理の際にバリデーションを追加しました。
     Changes to be committed:
    	modified:   src/app/Http/Controllers/HomeController.php
    	modified:   src/app/Http/Controllers/TaskController.php

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -24,8 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $login_user = Auth::user();
+        $user_id = $login_user->id;
         $task_model = new Task();
-        $tasks = $task_model->getWarningTask(1);
+        $tasks = $task_model->getWarningTask($user_id);
         return response()->json($tasks);
     }
 }
