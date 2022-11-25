f3348ed24dafe2ef962efae7965de7d1f612c3a7
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2022-11-25 12:19:55 +0900

    change: task seeder １．タスクシーダーのタイトル属性をスケジュールシーダーのタイトル属性と同様のものに変更しました。 ２．スケジュールシーダーのコードを整理しました。  Changes to be committed: 	modified:   src/database/seeders/ScheduleSeeder.php 	modified:   src/database/seeders/TaskSeeder.php

--- src/database/seeders/TaskSeeder.php
+++ src/database/seeders/TaskSeeder.php
@@ -17,6 +17,15 @@ class TaskSeeder extends Seeder
     public function run()
     {
 
+        // 対象
+        $objects = ['チーム', 'コード', 'テスト', 'メンバー', 'アプリ', 'SQL', 'バックエンド', 'フロントエンド', 'バグ', '仕様書', 'デザイン', 'SPA', '検索機能', 'ログイン機能', 'API認証', 'test.phpの62行目', 'JSOMデータの出力', 'プロフィール画面の更新ボタン', '進捗情報削除処理のテスト', 'タスク関連の作成機能', 'サイト訪問回数のグラフ', 'データの絞り込み機能と検索機能', 'ログインページのデザイン', '生徒側のエントリーボタンと取り消しボタン', 'ワークスペースに生徒を追加する機能', 'コンフリクト', 'エントリー登録と削除のバグ', 'ユーザシーダー', '生徒のマイグレーション', 'ログインコントローラー'];
+
+        // 助詞
+        $particle = ['を', 'は', 'に', 'が', 'も', 'から', 'より', 'と'];
+
+        // 動詞
+        $verb = ['作成する', '更新する', '削除する', '修正する', '変更する', '登録する', '入力する', '出力する', '追加する', 'テストする', 'リクエストする', '記述する', 'リファクタリングする', 'エラーが出ないように修正する', '開発する', '対応する'];
+
         $priority = ['low', 'middle', 'high'];
 
         // 特定の2つの日付の例
@@ -38,7 +47,7 @@ class TaskSeeder extends Seeder
 
             DB::table('task')->insert([
                 'id' => $i,
-                'title' => Str::random(rand(1, 63)),
+                'title' => $objects[rand(0, count($objects) - 1)] . $particle[rand(0, count($particle) - 1)] . $verb[rand(0, count($verb) - 1)],
                 'description' => Str::random(rand(0, 519)),
                 'priority' => $priority[rand(0, 2)],
                 'due' => $date,
