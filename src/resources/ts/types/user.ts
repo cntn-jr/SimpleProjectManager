48209729bff95209d3f0f342d47a009816a9eeda
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2022-11-28 10:47:59 +0900

    change: user type
    １．userのタイプをapiで取得する際に適応するように変更しました。
    ２．１に伴い、サインアップ時のみ使用するsignupUserタイプを作成しました。
     Changes to be committed:
    	modified:   src/resources/ts/components/molecules/SignForm.tsx
    	new file:   src/resources/ts/types/signupUser.ts
    	modified:   src/resources/ts/types/user.ts

--- src/resources/ts/types/user.ts
+++ src/resources/ts/types/user.ts
@@ -1,3 +1,10 @@
 export type User = {
-    
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    password? : string;
+    email_verified_at?: string;
+    created_at?: string | null;
+    updated_at?: string | null;
 }
