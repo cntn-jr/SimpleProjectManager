5a64d148d131c75318e843dffbdb6772e7060938
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2022-12-02 12:16:40 +0900

    add: private chat content
    １．PrivateChatContentのモデルやコントローラー、シーダー、マイグレーションファイルを作成しました。
    ２．PrivateChatRoomテーブルに属性を追加しました。
     Changes to be committed:
    	new file:   src/app/Http/Controllers/PrivateChatContentController.php
    	new file:   src/app/Models/PrivateChatContent.php
    	new file:   src/database/migrations/2022_12_02_030631_create_private_chat_contents_table.php
    	new file:   src/database/seeders/PrivateChatContentSeeder.php

---
+++ src/database/migrations/2022_12_02_030631_create_private_chat_contents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrivateChatContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('private_chat_contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('room_id');
+            $table->foreign('room_id')->references('id')->on('private_chat_rooms');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('private_chat_contents');
+    }
+}
