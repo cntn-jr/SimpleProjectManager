2d1734007dea983748552a16ebb81bc54de029eb
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2022-12-05 11:10:09 +0900

    change: Task Seeder
    １．TaskのSeederをFactoryを使用して追加するように変更しました。
     Changes to be committed:
    	new file:   src/database/factories/TaskFactory.php
    	modified:   src/database/seeders/TaskSeeder.php

---
+++ src/database/factories/TaskFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title_num = $this->faker->numberBetween(10, 30);
+        $priorities = ['low', 'middle', 'high'];
+        $prioritiy_num = $this->faker->numberBetween(0, 2);
+        return [
+            'title' => $this->faker->realText($title_num),
+            'description' => $this->faker->realText(50),
+            'priority' => $priorities[$prioritiy_num],
+            'due' => $this->faker->dateTimeBetween("-1month", "+1month"),
+            'user_id' => $this->faker->numberBetween(1, 11),
+            'is_finished' => $this->faker->boolean(30)
+        ];
+    }
+}
