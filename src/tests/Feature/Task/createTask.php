121fedfbe09e1217c502cfd3c4d2695971577932
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2022-12-13 11:29:53 +0900

    add: test create task
    １．タスク追加のAPIのテストを作成しました。
     Changes not staged for commit:
    	modified:   src/.gitignore

---
+++ src/tests/Feature/Task/createTask.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature\Task;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class createTask extends TestCase
+{
+    /**
+     * @test
+     */
+    public function createTaskByLoginUser()
+    {
+        // ユーザの作成
+        $user = User::factory()->create();
+        $user = User::find($user->id);
+        // タスク作成APIの実行
+        $response = $this->actingAs($user)->json("POST", "api/task/store", [
+            'title' => "title1",
+            'due' => "2022-03-06",
+            'priority' => "high",
+            'description' => "description1",
+        ]);
+        $response->assertStatus(200);
+        $response = $this->actingAs($user)->json("POST", "api/task/store", [
+            'title' => "title2",
+            'due' => "2022-12-11",
+            'priority' => "middle",
+            'description' => "description2",
+        ]);
+        $response->assertStatus(200);
+        $response = $this->actingAs($user)->json("POST", "api/task/store", [
+            'title' => "title3",
+            'due' => "2022-02-14",
+            'priority' => "low",
+            'description' => "",
+        ]);
+        $response->assertStatus(200);
+        // 上記のAPIで作成使用したデータがデータベースに存在するかチェック
+        $this->assertDatabaseHas('task', [
+            'title' => "title1",
+            'due' => "2022-03-06",
+            'priority' => "high",
+            'description' => "description1",
+        ]);
+        $this->assertDatabaseHas('task', [
+            'title' => "title2",
+            'due' => "2022-12-11",
+            'priority' => "middle",
+            'description' => "description2",
+        ]);
+        $this->assertDatabaseHas('task', [
+            'title' => "title3",
+            'due' => "2022-02-14",
+            'priority' => "low",
+            'description' => null,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function createTaskByNotLoginUser()
+    {
+        // タスク作成APIの実行
+        $response = $this->json("POST", "api/task/store", [
+            'title' => "title1",
+            'due' => "2022-03-06",
+            'priority' => "high",
+            'description' => "description1",
+        ]);
+        $response->assertStatus(401);
+    }
+}
