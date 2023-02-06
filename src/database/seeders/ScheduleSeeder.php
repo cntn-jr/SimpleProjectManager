b6ca39ee888821e3679e199c0b29a8bc6fa0a0eb
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-02-06 09:53:52 +0900

    update: schedule seeder
    ・予定のデータを本番環境で使用するデータに近づけました。

--- src/database/seeders/ScheduleSeeder.php
+++ src/database/seeders/ScheduleSeeder.php
@@ -17,6 +17,14 @@ class ScheduleSeeder extends Seeder
      */
     public function run()
     {
-        Schedule::factory()->count(30)->create();
+        // Schedule::factory()->count(30)->create();
+        Schedule::factory()->create(["title" => "単体テストの完成"]);
+        Schedule::factory()->create(["title" => "結合テストの完成"]);
+        Schedule::factory()->create(["title" => "フロントエンドの完成"]);
+        Schedule::factory()->create(["title" => "バックエンドの完成"]);
+        Schedule::factory()->create(["title" => "ユーザテストの実施"]);
+        Schedule::factory()->create(["title" => "使用するライブラリの決定"]);
+        Schedule::factory()->create(["title" => "要件定義書の作成"]);
+        Schedule::factory()->create(["title" => "設計書の作成"]);
     }
 }
