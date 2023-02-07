409d2009a5a2689cf9b0b1b1e74456f06d3f6f6b
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-02-07 12:17:35 +0900

    update: due of task on factory
    ・タスクのファクトリー上のdue属性の日時範囲を狭めました。

--- src/database/factories/TaskFactory.php
+++ src/database/factories/TaskFactory.php
@@ -24,7 +24,7 @@ class TaskFactory extends Factory
             'title' => $this->faker->realText($title_num),
             'description' => $this->faker->realText(50),
             'priority' => $priorities[$prioritiy_num],
-            'due' => $this->faker->dateTimeBetween("-1month", "+1month"),
+            'due' => $this->faker->dateTimeBetween("-15day", "+1month"),
             // 'user_id' => $this->faker->numberBetween(1, 11),
             'user_id' => $this->faker->numberBetween(2, 11),
             'is_finished' => $this->faker->boolean(30)
